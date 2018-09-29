1adddbf78f802cfbbbdaa49dd7fd958faae26496
Morris Arroyo <arroyomorris28@gmail.com>
2018-09-29 13:30:16 -0700

    imported dragon from Fantasy Pack

---
+++ Assets/Scripts/Dragon.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dragon : MonoBehaviour, BaseCreature
+{
+    public int health { get; set; }
+    public int basicAttackDamage { get; set; }
+    public int movementSpeed { get; set; }
+
+    Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void BasicAttack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Move()
+    {
+
+    }
+
+    public void TakeDamage(int damage)
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
