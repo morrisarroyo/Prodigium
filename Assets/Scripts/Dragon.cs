c10cd703afde8f28d8467eaa5875a3a80fe7bdc1
Morris Arroyo <arroyomorris28@gmail.com>
2018-10-09 12:52:29 -0700

    add Dragon Attack

--- Assets/Scripts/Dragon.cs
+++ Assets/Scripts/Dragon.cs
@@ -5,19 +5,27 @@ using UnityEngine;
 public class Dragon : MonoBehaviour, BaseCreature
 {
     public const string tag = "Dragon";
+    const string isWalkingStr = "IsWalking";
+    const string isTurningStr = "IsTurning";
+    const string basicAttack = "BasicAttack";
+
 
     public int health;
     public int basicAttackDamage;
     public float movementSpeed = 6f;
-    public float rotationSpeed = 6f;
+    public float rotationSpeed = 3f;
+
+    public List<string> actions;
 
     GameObject player;
     Rigidbody rb;
+    Animator anim;
 
     // Use this for initialization
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
 
         player = getPlayerToTrack();
     }
@@ -26,11 +34,13 @@ public class Dragon : MonoBehaviour, BaseCreature
     void Update()
     {
         Move();
+        //BasicAttack();
     }
 
     public void BasicAttack()
     {
-        throw new System.NotImplementedException();
+        if (Vector3.Distance(transform.position, player.transform.position) < 1.0f)
+            TriggerBasicAttack();
     }
 
     public void Move()
@@ -42,13 +52,52 @@ public class Dragon : MonoBehaviour, BaseCreature
             float rotationStep = rotationSpeed * Time.deltaTime;
 
             // Move our position a step closer to the target.
+            if (!transform.position.Equals(player.transform.position))
+            {
+                transform.position = Vector3.MoveTowards(transform.position, player.transform.position, movementStep);
 
-            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, movementStep);
-            Vector3 targetDir = player.transform.position - transform.position;
-            Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, rotationStep, 0f);
-            transform.rotation = Quaternion.LookRotation(newDir);
+                StartMovementAnimation();
+
+                Vector3 targetDir = player.transform.position - transform.position;
+                Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, rotationStep, 0f);
+                transform.rotation = Quaternion.LookRotation(newDir);
+
+            }
+            else
+            {
+                StopMovementAnimation();
+            }
         }
     }
+
+
+    private void StartMovementAnimation()
+    {
+        anim.SetBool(isWalkingStr, true);
+    }
+
+    private void StopMovementAnimation()
+    {
+        anim.SetBool(isWalkingStr, false);
+    }
+
+    private void TriggerBasicAttack()
+    {
+        anim.SetTrigger(basicAttack);
+    }
+    /*
+    private void StartTurningAnimation()
+    {
+        anim.SetBool(isTurningStr, true);
+    }
+
+    private void StopTurningAnimation()
+    {
+        anim.SetBool(isTurningStr, false);
+    }
+    */
+
+
 
     public void TakeDamage(int damage)
     {
